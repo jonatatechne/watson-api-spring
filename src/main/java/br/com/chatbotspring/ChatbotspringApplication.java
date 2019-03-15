95332497c4f8433cabeadb6a9595cd3f6e77e3cd
Jonata Hessa <jonata.hessa@gmail.com>
2019-03-15 14:54:25 -0300

    first commit

---
+++ src/main/java/br/com/chatbotspring/ChatbotspringApplication.java
@@ -0,0 +1,13 @@
+package br.com.chatbotspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatbotspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatbotspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/chatbotspring/ChatbotspringApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.chatbotspring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ChatbotspringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
